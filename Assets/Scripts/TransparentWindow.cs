86236de86623acfa7942969beae1e3d5c80e4ebd
Sophia Akhavan <sophiaakhavan19@gmail.com>
2025-02-23 14:54:18 -0800

    in progress transparent window
    code monkey video https://youtu.be/RqgsGaMPZTw?t=299

---
+++ Assets/Scripts/TransparentWindow.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparentWindow : MonoBehaviour
+{
+    [DllImport("user32.dll")]
+    public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+    private void Start()
+    {
+        MessageBox(new IntPtr(0), "Hello World!", "Hello Dialog", 0);
+    }
+}
